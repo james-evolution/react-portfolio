39357f61b35fd57f925da7f526205ef39a069701
Robert Alkire <jalkire1776@gmail.com>
2023-09-26 17:18:42 -0600

    Added resume download.

--- components/gridcontainer.js
+++ components/gridcontainer.js
@@ -12,6 +12,8 @@ import { Divider, Typography } from "@mui/material";
 import Banner from "./banner";
 import Contact from "./contact";
 import { Stack } from "@mui/material";
+import Button from "@mui/material/Button";
+import DownloadIcon from "@mui/icons-material/Download";
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor:
@@ -28,45 +30,57 @@ function GridContainer() {
     return (
         <Box className={classes.gridbox} sx={{ flexGrow: 1, height: "100vw" }}>
             <Grid container spacing={2}>
-                    {/* Carousel */}
-                    <Grid className={classes.carouselGrid} xs={12}>
-                        <Banner />
+                {/* Carousel */}
+                <Grid className={classes.carouselGrid} xs={12}>
+                    <Banner />
+                </Grid>
+
+                {/* About (Fluencies | About Me | Technologies) */}
+                <Grid className={classes.about} xs={12}>
+                    <About id="About" />
+                </Grid>
+
+                {/* Projects  */}
+                <Grid
+                    className={classes.projects}
+                    xs={12}
+                    sx={{ marginTop: "32px" }}
+                >
+                    <Divider id="#Projects" sx={{ color: "white" }}>
+                        <Typography variant="h4" component="h2">
+                            Projects
+                        </Typography>
+                    </Divider>
+
+                    <Grid className={classes.projects} xs={12}>
+                        <Projects id="Projects" />
                     </Grid>
 
-                    {/* About (Fluencies | About Me | Technologies) */}
-                    <Grid className={classes.about} xs={12}>
-                        <About id="About" />
+                    <Grid className={classes.projects} xs={12} sx={{textAlign: "center", paddingTop: "12px", paddingBottom: "12px"}}>
+                        <a href="/Jeremy_Alkire_Resume_2023.pdf" download>
+                            <Button
+                                component="label"
+                                variant="contained"
+                                startIcon={<DownloadIcon />}
+                            >
+                                Download Resume
+                            </Button>
+                        </a>
                     </Grid>
 
-                    {/* Projects  */}
-                    <Grid
-                        className={classes.projects}
-                        xs={12}
-                        sx={{ marginTop: "32px" }}
+                    <Divider
+                        id="Contact"
+                        sx={{ marginTop: "24px", color: "white" }}
                     >
-                        <Divider id="#Projects" sx={{ color: "white" }}>
-                            <Typography variant="h4" component="h2">
-                                Projects
-                            </Typography>
-                        </Divider>
+                        <Typography variant="h4" component="h2">
+                            Contact Information
+                        </Typography>
+                    </Divider>
 
-                        <Grid className={classes.projects} xs={12}>
-                            <Projects id="Projects" />
-                        </Grid>
-
-                        <Divider
-                            id="Contact"
-                            sx={{ marginTop: "24px", color: "white" }}
-                        >
-                            <Typography variant="h4" component="h2">
-                                Contact Information
-                            </Typography>
-                        </Divider>
-
-                        <Grid className={classes.projects} xs={12}>
-                            <Contact />
-                        </Grid>
+                    <Grid className={classes.projects} xs={12}>
+                        <Contact />
                     </Grid>
+                </Grid>
             </Grid>
         </Box>
     );
